4ad4cc93730b871cfb8c2a0be96d097c85120671
Md Tufail <138331429+Tufail-Prosolver@users.noreply.github.com>
2023-12-19 16:21:46 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+        int n = 4;
+        int m = 5;
+
+        for(int i = 1; i<=n; i++){
+
+            for(int j = 1; j<=i; j++ ){
+                printf("*"); 
+            }
+             printf("\n");
+}
+
+    return 0;
+}
